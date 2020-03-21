176b626008a6580f6a2a785adece12f549baabc4
Terence Ng Han How <ngtere@live.com.sg>
2020-03-21 19:43:51 +1100

    Fixed bug thats made me go crazy

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const image = require("./controllers/image");
 
 const db = knex({
   client: "pg",
-  connectionString: {
-    host: process.env.DATABASE_URL,
+  connection: {
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 });
